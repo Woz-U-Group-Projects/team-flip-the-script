1b222fddecaa1ac5cfeadd6f0e3cdc51f952df81
TheRealGeek <mcparnell94@gmail.com>
2019-02-18 17:15:44 -0800

    Added handlebars routing, verified that the routes work with the new changes. Added necessary files, edited the package.json to require only what we need (verified with a deletion of the node_modules folder and then a fresh npm installation). The new port location is now 'http://localhost:3000/' This can potentially be changed later, but let's only address that if we need to. The handlebars integration broke some of the css styling, will need to be fixed before the end of the week.

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const inputRouter = require('./routes/input');
 
-const user = encodeURIComponent('admin');
-const password = encodeURIComponent('D5G2cZ9k');
-const authMechanism = 'DEFAULT';
+const app = express();
 
-// Connection URL
-const url = `mongodb://${user}:${password}@localhost:27017/?authMechanism=${authMechanism}`;
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
-// Create a new MongoClient
-const client = new MongoClient(url);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Use connect method to connect to the Server
-client.connect(function (err) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
 
-  client.close();
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/input', inputRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ cover/public/js/calculator.js
@@ -0,0 +1,115 @@
+/*This is a rough draft of what I am wanting to do for the actual budgeting option */
+alert("calculator.js connected");
+//Addition calculator function
+function add(a, b) {
+  return a + b;
+}
+//------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+/* Candidate for a constructor class?
+idk, we'll see how the app comes together
+*/
+
+//Income category
+var income = [];
+//Household expense category
+var living = [];
+//Debt expense category
+var debt = [];
+//Experiences expense category
+var experiences = [];
+// Transportation expense category
+var transportation = [];
+//Wealthbuilding expense category
+var wealthbuilding = [];
+//Health expense category
+var health = [];
+//------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+
+//income Total:
+const iT = income.reduce(add, 0);
+//Expense Living Total:
+const elT = living.reduce(add, 0);
+//Expense Debt Total:
+const edT = debt.reduce(add, 0);
+//Expense Experiences total:
+const eeT = experiences.reduce(add, 0);
+//Expense Transportation total:
+const etT = transportation.reduce(add, 0);
+//Expense Wealthbuilding total:
+const ewT = wealthbuilding.reduce(add, 0);
+//Expense Health total:
+const ehT = health.reduce(add, 0);
+//Expense Grand Total:
+const Expense = (elT + edT + edT + eeT + etT + ewT + ehT);
+
+
+console.log("Total Income sources are " + iT);
+console.log("Total Living expenses are " + elT);
+console.log("Total Debt expenses are " + edT);
+console.log("Total Experiences Expenses are " + eeT);
+console.log("Total Transportation Expenses are " + etT);
+console.log("Total Wealthbuilding Expenses are " + ewT);
+console.log("Total Health Expenses are " + ehT);
+console.log("Total Expenses are: " + Expense);
+//------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+
+var jstoday = new Date();
+// console.log("Today's date is " + today + " and the day is " + stringDay );
+//So this works, so long as "a" parameter entered is a javascript date format
+function readableDate(a){
+  var dd = a.getDate();
+  var mm = a.getMonth() + 1; //January is 0!
+  var yyyy = a.getFullYear();
+  var day = a.getDay();//Sunday is 0
+  var readableDate;
+  var stringDay;
+  if (dd < 10) {
+    dd = '0' + dd;
+  }
+  if (mm < 10) {
+    mm = '0' + mm;
+  }
+  readableDate = mm + '/' + dd + '/' + yyyy;
+  if (day === 0) {
+    stringDay = "Sunday";
+  }
+  if (day === 1) {
+    stringDay = "Monday";
+  }
+  if (day === 2) {
+    stringDay = "Tuesday";
+  }
+  if (day === 3) {
+    stringDay = "Wednesday";
+  }
+  if (day === 4) {
+    stringDay = "Thursday";
+  }
+  if (day === 5) {
+    stringDay = "Friday";
+  }
+  if (day === 6) {
+    stringDay = "Saturday";
+  }
+  console.log("The date is " + readableDate + ", and the day of the week is " + stringDay);
+return stringDay + readableDate;
+}
+//make this a function with a 
+var plusTwoWeeks = new Date(+new Date + 12096e5);
+console.log(plusTwoWeeks); //this gives a date two weeks from today in JS format
+readableDate(jstoday);
+readableDate(plusTwoWeeks);
+var d = new Date(2018, 11, 4);
+console.log(d);
