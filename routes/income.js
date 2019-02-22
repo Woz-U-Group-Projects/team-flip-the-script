36bdab1b4f2b55db21debed9b2f447e4a85ec3ee
TheRealGeek <mcparnell94@gmail.com>
2019-02-21 21:42:17 -0800

    fixing the income.js and income.hbs files to remove extra data

--- routes/income.js
+++ routes/income.js
@@ -7,4 +7,48 @@ router.get('/', function (req, res, next) {
     });
 });
 
+
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'Amazing US Cities',
+        cities: [{
+                name: 'San Fransisco',
+                state: 'CA',
+                population: 864816
+            },
+            {
+                name: 'Denver',
+                state: 'CO',
+                population: 682545
+            },
+            {
+                name: 'Seattle',
+                state: 'WA',
+                population: 704352
+            },
+            {
+                name: 'Portland',
+                state: 'OR',
+                population: 639863
+            },
+            {
+                name: 'New York City',
+                state: 'NY',
+                population: 8.538
+            },
+            {
+                name: 'Charlotte',
+                state: 'NC',
+                population: 842051
+            },
+            {
+                name: 'Pittsburgh',
+                state: 'PA',
+                population: 303625
+            }
+        ]
+    });
+});
+
+
 module.exports = router;
